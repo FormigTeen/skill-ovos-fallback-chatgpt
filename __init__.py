9191abbd0d9630c971fc231e31af16e816428644
Matheus Freitas <msformigteen@live.com>
2024-06-02 10:45:29 -0300

    refact the code and use official lib

--- __init__.py
+++ __init__.py
@@ -1,18 +1,10 @@
-from ovos_bus_client.session import SessionManager
-from ovos_solver_openai_persona import OpenAIPersonaSolver
 from ovos_utils import classproperty
 from ovos_utils.process_utils import RuntimeRequirements
 from ovos_workshop.skills.fallback import FallbackSkill
-
-
-DEFAULT_SETTINGS = {
-    "persona": "You are a helpful voice assistant with a friendly tone and fun sense of humor. You respond in 40 words or fewer.",
-    "model": "gpt-3.5-turbo",
-}
-
+from ovos_config import Configuration
+from openai import OpenAI
 
 class ChatGPTSkill(FallbackSkill):
-    sessions = {}
 
     @classproperty
     def runtime_requirements(self):
@@ -24,89 +16,105 @@ class ChatGPTSkill(FallbackSkill):
         )
 
     def initialize(self):
-        self.settings.merge(DEFAULT_SETTINGS, new_only=True)
         self.add_event("speak", self.handle_speak)
         self.add_event("recognizer_loop:utterance", self.handle_utterance)
         self.register_fallback(self.ask_chatgpt, 85)
 
     @property
+    def config(self):
+        return Configuration().get('chatgpt' ,{})
+
+    @property
     def ai_name(self):
-        return self.settings.get("name", "Chat G.P.T.")
+        return self.config.get("name", "Chat G.P.T.")
 
     @property
     def confirmation(self):
-        return self.settings.get("confirmation", True)
+        return self.config.get("confirmation", False)
+
+    @property
+    def key(self):
+        return self.config.get("key")
+
+    @property
+    def thread_id(self):
+        return self.config.get("thread_id")
+
+    @property
+    def assistant_id(self):
+        return self.config.get("assistant_id")
+
+    @property
+    def client(self):
+        """created fresh to allow key/url rotation when settings.json is edited"""
+        try:
+            return OpenAI(
+                api_key=self.key
+            )
+        except Exception as err:
+            self.log.error(err)
+            return None
 
     @property
     def chat(self):
-        """created fresh to allow key/url rotation when settings.json is edited"""
         try:
-            return OpenAIPersonaSolver(config=self.settings)
+            return self.client.beta.threads.runs.create(
+                thread_id=self.thread_id,
+                assistant_id=self.assistant_id,
+                stream=True
+            )
         except Exception as err:
             self.log.error(err)
             return None
 
     def handle_utterance(self, message):
-        utt = message.data.get("utterances")[0]
-        sess = SessionManager.get(message)
-        if sess.session_id not in self.sessions:
-            self.sessions[sess.session_id] = []
-        self.sessions[sess.session_id].append(("user", utt))
+        utt = message.data.get("utterance")
+        #print(utt)
+        #self.create_message(utt, role="user")
 
     def handle_speak(self, message):
         utt = message.data.get("utterance")
-        sess = SessionManager.get(message)
-        if sess.session_id in self.sessions:
-            self.sessions[sess.session_id].append(("ai", utt))
+        print(message.data)
+        ##self.create_message(utt, role="ovos-ai")
 
-    def build_msg_history(self, message):
-        sess = SessionManager.get(message)
-        if sess.session_id not in self.sessions:
-            return []
-        messages = []  # tuple of question, answer
-
-        q = None
-        ans = None
-        for m in self.sessions[sess.session_id]:
-            if m[0] == "user":
-                q = m[1]  # track question
-                if ans is not None:
-                    # save previous q/a pair
-                    messages.append((q, ans))
-                    q = None
-                ans = None
-            elif m[0] == "ai":
-                if ans is None:
-                    ans = m[1]  # track answer
-                else:  # merge multi speak answers
-                    ans = f"{ans}. {m[1]}"
-
-        # save last q/a pair
-        if ans is not None and q is not None:
-            messages.append((q, ans))
-        return messages
-
+    def create_message(self, message, role):
+        if "key" not in self.config or "assistant_id" not in self.config or "thread_id" not in self.config:
+            self.log.error(
+                "ChatGPT not configured yet, please set your API key in settings.json",
+            )
+        return self.client.beta.threads.messages.create(
+            self.thread_id,
+            role=role,
+            content=message
+        )
     def _async_ask(self, message):
         utterance = message.data["utterance"]
-        self.chat.qa_pairs = self.build_msg_history(message)
+        self.create_message(utterance, role="user")
         answered = False
         try:
-            for utt in self.chat.stream_utterances(utterance):
-                answered = True
-                self.speak(utt)
+            for chunk in self.chat:
+                if chunk:
+                    if chunk.event == "thread.run.completed":
+                        answered = True
+                    if chunk.event == "thread.message.completed":
+                        answered = True
+                        for message in chunk.data.content:
+                            None
+                            self.speak(message.text.value)
+                    if chunk.event == "thread.run.failed":
+                        raise Exception("Error on server")
         except Exception as err:  # speak error on any network issue / no credits etc
             self.log.error(err)
         if not answered:
             self.speak_dialog("gpt_error", data={"name": self.ai_name})
 
     def ask_chatgpt(self, message):
-        if "key" not in self.settings:
+        utterance = message.data["utterance"]
+        if "key" not in self.config or "assistant_id" not in self.config or "thread_id" not in self.config:
             self.log.error(
-                "ChatGPT not configured yet, please set your API key in %s",
-                self.settings.path,
+                "ChatGPT not configured yet, please set your API key in settings.json",
             )
             return False  # ChatGPT not configured yet
-        utterance = message.data["utterance"]
         if self.confirmation:
             self.speak_dialog("asking", data={"name": self.ai_name})
         # ask in a thread so fallback doesnt timeout
